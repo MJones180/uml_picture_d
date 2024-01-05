bf7b8edb0a0da0e5913c9979d379e5dab2135fbc
Michael Jones <jonesmichael3.1415@gmail.com>
2024-01-05 16:17:42 -0500

    Updated the terminate util

--- src/utils/terminate_with_message.py
+++ src/utils/terminate_with_message.py
@@ -2,12 +2,25 @@ import sys
 
 
 def terminate_with_message(message):
-    print(f'''
-{'-' * 18}
-TERMINATING SCRIPT
+    """Terminate the current process and display a message to the user.
 
-REASON:
-  {message}
-{'-' * 18}
-''')
+    Parameters
+    ----------
+    message : str
+        Message to write out to the CLI.
+
+    Notes
+    -----
+    This function will terminate the process via `sys.exit()`.
+    """
+
+    print()
+    print('-' * 22)
+    print('- TERMINATING SCRIPT -')
+    print('-' * 22)
+    print()
+    print('REASON:')
+    print(f'  {message}')
+    print()
+    print('END.')
     sys.exit()
