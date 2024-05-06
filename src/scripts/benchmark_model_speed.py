ba3b9a31d1b0209c4f1fa1f23ecca1edeac05390
Michael Jones <jonesmichael3.1415@gmail.com>
2024-05-06 10:24:11 -0400

    Added a util to benchmark a model

---
+++ src/scripts/benchmark_model_speed.py
@@ -0,0 +1,76 @@
+"""
+As of right now, this script will test the speed of just one row.
+"""
+
+import time
+import torch
+from utils.load_model import LoadModel
+from utils.norm import min_max_denorm
+
+
+def benchmark_model_speed_parser(subparsers):
+    """
+    Example commands:
+    python3 main.py benchmark_model_speed v1a 110
+    """
+    subparser = subparsers.add_parser(
+        'benchmark_model_speed',
+        help='test a trained model',
+    )
+    subparser.set_defaults(main=benchmark_model_speed)
+    subparser.add_argument(
+        'tag',
+        help='tag of the model, will look in `/output/trained_models`',
+    )
+    subparser.add_argument(
+        'epoch',
+        help='epoch of the trained model to test (just the number part)',
+    )
+    subparser.add_argument(
+        '--iterations',
+        default=1000,
+        help='number of iterations to perform',
+    )
+
+
+def benchmark_model_speed(cli_args):
+    tag = cli_args['tag']
+    epoch = cli_args['epoch']
+    iterations = cli_args['iterations']
+
+    model_loader = LoadModel(tag, epoch, eval_mode=True)
+    model = model_loader.get_model()
+    network = model_loader.get_network()
+    norm_values = model_loader.get_norm_values()
+
+    output_max_min_diff = norm_values['output_max_min_diff']
+    output_min_x = norm_values['output_min_x']
+
+    example_input = network.example_input()
+
+    def _run_model():
+        with torch.no_grad():
+            model_output = model(example_input).numpy()
+        return model_output
+
+    def _run_model_and_denorm():
+        model_output = _run_model()
+        min_max_denorm(
+            model_output,
+            output_max_min_diff,
+            output_min_x,
+        )
+
+    def _benchmark(func):
+        total_time = 0
+        for i in range(iterations):
+            start = time.time()
+            func()
+            finish = time.time()
+            total_time += finish - start
+        print(total_time / iterations)
+
+    print('Average time for the nn to process one row:')
+    _benchmark(_run_model)
+    print('Average time for nn to process one row + denorm:')
+    _benchmark(_run_model_and_denorm)
