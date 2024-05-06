65bbad51a2ae428c5f3923b815da4d5ba52a05d7
Michael Jones <jonesmichael3.1415@gmail.com>
2024-05-06 10:39:10 -0400

    Added wrapper for grid search script

---
+++ src/scripts/grid_search_train_and_test.py
@@ -0,0 +1,27 @@
+"""
+Depending on how this is configured
+"""
+
+
+def grid_search_parser(subparsers):
+    """
+    Example commands:
+    ...
+    """
+    subparser = subparsers.add_parser(
+        'grid_search_train_and_test',
+        help='grid search train models ',
+    )
+    subparser.set_defaults(main=grid_search_train_and_test)
+    subparser.add_argument(
+        'argname',
+        help='help',
+    )
+    subparser.add_argument(
+        '--spawn-new-jobs',
+        help='spawn new jobs for each training and testing call',
+    )
+
+
+def grid_search_train_and_test(cli_args):
+    pass
