202614e9e146ec469d26d421f479230c10add42d
Michael Jones <jonesmichael3.1415@gmail.com>
2024-05-07 14:11:22 -0400

    Removing the explicit grid search script for now

--- src/script_parsers.py
+++ src/script_parsers.py
@@ -2,7 +2,6 @@ from scripts.batch_model_test import batch_model_test_parser
 from scripts.batch_model_train import batch_model_train_parser
 from scripts.benchmark_model_speed import benchmark_model_speed_parser
 from scripts.dataset_info import dataset_info_parser
-from scripts.grid_search_train_and_test import grid_search_parser
 from scripts.model_test import model_test_parser
 from scripts.model_train import model_train_parser
 from scripts.network_info import network_info_parser
@@ -13,7 +12,6 @@ script_parsers = [
     batch_model_train_parser,
     benchmark_model_speed_parser,
     dataset_info_parser,
-    grid_search_parser,
     model_test_parser,
     model_train_parser,
     network_info_parser,
