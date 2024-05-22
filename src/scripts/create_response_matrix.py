5513f46371c6a45a4cad68f74216f5dc35e12277
Michael Jones <jonesmichael3.1415@gmail.com>
2024-05-22 15:50:14 -0400

    Adding the script to generate a response matrix

---
+++ src/scripts/create_response_matrix.py
@@ -0,0 +1,33 @@
+"""
+Create a response matrix.
+"""
+
+from utils.constants import DATA_F, PROPER_SIM_DATA_P
+from utils.printing_and_logging import step_ri, title
+
+
+def create_response_matrix_parser(subparsers):
+    """
+    Example command:
+        python3 main.py create_response_matrix --simulated-data-tag ds_fixed_10nm
+    """
+    subparser = subparsers.add_parser(
+        'create_response_matrix',
+        help='simulate data using PROPER',
+    )
+    subparser.set_defaults(main=create_response_matrix)
+    subparser.add_argument(
+        '--simulated-data-tag',
+        help=('generate the response matrix from simulated data, the data '
+              'should be simulated via the `sim_data` script with the '
+              '`--fixed-amount-per-zernike` argument passed'),
+    )
+
+
+def create_response_matrix(cli_args):
+    title('Create response matrix script')
+
+    simulated_data_tag = cli_args['simulated_data_tag']
+    if simulated_data_tag:
+        datafile_path = f'{PROPER_SIM_DATA_P}/{simulated_data_tag}/{DATA_F}'
+        print(datafile_path)
