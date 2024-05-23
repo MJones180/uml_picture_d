b97e87596b0479be9d8bb0350f96858abc847f30
Michael Jones <jonesmichael3.1415@gmail.com>
2024-05-23 10:33:40 -0400

    Added code to generate response matrix

--- src/scripts/create_response_matrix.py
+++ src/scripts/create_response_matrix.py
@@ -1,9 +1,23 @@
 """
 Create a response matrix.
+
+For an image, we can describe it by the following:
+    I (image's intensity field) = M (response matrix) * Z (Zernike coefficients)
+The response matrix, M, is the Jacobian which we can solve for by using finite
+differencing. Each element is given by the equation:
+    (delta intensity field) / (delta Zernike perturbation)
+where the intensity field has its pixels flattened out into the rows and the
+Zernike terms are each of the columns.
+By inverting M, we can then solve for the Zernike coefficients present in a
+given image by the following:
+    Z = M_inv * I
 """
 
-from utils.constants import DATA_F, RAW_SIMULATED_DATA_P
+import numpy as np
+from utils.constants import DATA_F, RAW_SIMULATED_DATA_P, RESPONSE_MATRICES_P
+from utils.hdf_read_and_write import HDFWriteModule, read_hdf
 from utils.printing_and_logging import step_ri, title
+from utils.terminate_with_message import terminate_with_message
 
 
 def create_response_matrix_parser(subparsers):
@@ -27,7 +41,53 @@ def create_response_matrix_parser(subparsers):
 def create_response_matrix(cli_args):
     title('Create response matrix script')
 
+    step_ri('Loading in the data')
     simulated_data_tag = cli_args['simulated_data_tag']
     if simulated_data_tag:
+        print(f'Grabbing the data from simulated dataset {simulated_data_tag}')
         datafile_path = f'{RAW_SIMULATED_DATA_P}/{simulated_data_tag}/{DATA_F}'
-        print(datafile_path)
+        data = read_hdf(datafile_path)
+
+        # The shape of this data is (fields, pixels, pixels)
+        intensity = data['ccd_intensity'][:]
+        # The shape should be converted to (flattened_pixels, fields)
+        intensity = intensity.reshape(intensity.shape[0], -1).T
+        # The last column of data is the intensity field without any Zernike
+        # aberrations, so we will take our differences with respect to it
+        base_field = intensity[:, -1]
+        # All the columns now consist of perturbed fields for each Zernike term
+        perturbation_fields = intensity[:, :-1]
+        # For the perturbation amounts, the last row is for the base case (it
+        # has no aberrations), so we can chop it off
+        perturbation_amounts = data['zernike_coeffs'][:-1]
+
+    step_ri('Calculating M')
+    # This is the delta intensity field portion
+    field_changes = perturbation_fields - base_field[:, None]
+
+    # Verify that all the perturbations are equal
+    perturbation_amounts_diag = np.diag(perturbation_amounts)
+    if not np.all(perturbation_amounts_diag == perturbation_amounts_diag[0]):
+        terminate_with_message('All Zernike perturbation amounts must be '
+                               'the same')
+    else:
+        perturbation_amount = perturbation_amounts_diag[0]
+
+    # Verify that all off-diagonal elements are zero, so set all main diagonal
+    # elements to zero to make the check easier
+    perturbation_amounts[np.diag_indices(perturbation_amounts.shape[0])] = 0
+    if not np.all(perturbation_amounts == 0):
+        terminate_with_message('There are off diagonal Zernike perturbations')
+
+    # The perturbation amount should be fixed for all terms
+    M_matrix = field_changes / perturbation_amount
+
+    step_ri('Calculating M_inv')
+    M_matrix_inv = np.linalg.pinv(M_matrix)
+
+    step_ri('Saving M_inv')
+    output_path = f'{RESPONSE_MATRICES_P}/{simulated_data_tag}'
+    print(f'Outputting to {output_path}')
+    HDFWriteModule(output_path).create_and_write_hdf_simple({
+        'response_matrix': M_matrix_inv,
+    })
