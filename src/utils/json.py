241969186616b502e95742e68a06dfabda9cc4ed
Michael Jones <jonesmichael3.1415@gmail.com>
2024-05-23 12:46:17 -0400

    Fixed a bug in writing to JSON

--- src/utils/json.py
+++ src/utils/json.py
@@ -9,6 +9,8 @@ class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
+        elif isinstance(obj, np.floating):
+            return float(obj)
         return json.JSONEncoder.default(self, obj)
 
 
