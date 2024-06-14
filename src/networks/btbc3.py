3d71e53bb80f1aedc1fe3966875d75285727e7b2
Michael Jones <jonesmichael3.1415@gmail.com>
2024-06-14 12:55:22 -0400

    Added 2 new networks

---
+++ src/networks/btbc3.py
@@ -0,0 +1,61 @@
+# Back to Back Convolution 3 (btbc3)
+# Trainable parameters: 3,415,767
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Network(nn.Module):
+
+    def example_input():
+        return torch.rand((1, 1, 32, 32))
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 8, 3)
+        self.dropout1 = nn.Dropout(0.1)
+        self.conv2 = nn.Conv2d(8, 16, 3)
+        self.dropout2 = nn.Dropout(0.1)
+        self.maxpool1 = nn.MaxPool2d(2)
+        self.conv3 = nn.Conv2d(16, 32, 3)
+        self.dropout3 = nn.Dropout(0.1)
+        self.conv4 = nn.Conv2d(32, 64, 3)
+        self.dropout4 = nn.Dropout(0.1)
+        self.conv5 = nn.Conv2d(64, 128, 3)
+        self.dropout5 = nn.Dropout(0.1)
+        self.conv6 = nn.Conv2d(128, 256, 3)
+        self.dropout6 = nn.Dropout(0.1)
+        self.maxpool2 = nn.MaxPool2d(2)
+        self.flattened1 = nn.Linear(2304, 1024)
+        self.dropout7 = nn.Dropout(0.4)
+        self.flattened2 = nn.Linear(1024, 512)
+        self.dropout8 = nn.Dropout(0.4)
+        self.flattened3 = nn.Linear(512, 256)
+        self.dropout9 = nn.Dropout(0.4)
+        self.flattened4 = nn.Linear(256, 23)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.dropout1(x)
+        x = F.relu(self.conv2(x))
+        x = self.dropout2(x)
+        x = self.maxpool1(x)
+        x = F.relu(self.conv3(x))
+        x = self.dropout3(x)
+        x = F.relu(self.conv4(x))
+        x = self.dropout4(x)
+        x = F.relu(self.conv5(x))
+        x = self.dropout5(x)
+        x = F.relu(self.conv6(x))
+        x = self.dropout6(x)
+        x = self.maxpool2(x)
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.flattened1(x))
+        x = self.dropout7(x)
+        x = F.relu(self.flattened2(x))
+        x = self.dropout8(x)
+        x = F.relu(self.flattened3(x))
+        x = self.dropout9(x)
+        x = self.flattened4(x)
+        return x
