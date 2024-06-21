9d1e6e15433d1cb673929a0115bd1d1a5c2e4e4c
Michael Jones <jonesmichael3.1415@gmail.com>
2024-06-21 11:20:08 -0400

    Added three dense networks with no dropout

---
+++ src/networks/dfc3n.py
@@ -0,0 +1,29 @@
+# Dense Fully Connected 3 (dfc3n)
+# Trainable parameters: 4,790,807
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Network(nn.Module):
+
+    def example_input():
+        return torch.rand((1, 1, 32, 32))
+
+    def __init__(self):
+        super().__init__()
+        self.flattened1 = nn.Linear(1024, 2048)
+        self.flattened2 = nn.Linear(2048, 1024)
+        self.flattened3 = nn.Linear(1024, 512)
+        self.flattened4 = nn.Linear(512, 128)
+        self.flattened5 = nn.Linear(128, 23)
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.flattened1(x))
+        x = F.relu(self.flattened2(x))
+        x = F.relu(self.flattened3(x))
+        x = F.relu(self.flattened4(x))
+        x = self.flattened5(x)
+        return x
