381b2d2d17139ed8e8b1396b3eab0047b034970d
Michael Jones <jonesmichael3.1415@gmail.com>
2024-08-07 10:16:19 -0400

    Set default arg to remove warning

--- src/utils/model.py
+++ src/utils/model.py
@@ -70,7 +70,7 @@ class Model():
         self.network = load_network(self.network_name)
         self.model = self.network().to(self.device)
         # Now, the weights can be set
-        self.model.load_state_dict(torch.load(model_path))
+        self.model.load_state_dict(torch.load(model_path, weights_only=False))
         # Set to evaluation mode
         self.model.eval()
         dec_print_indent()
