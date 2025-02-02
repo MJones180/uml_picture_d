67679bb5c573d3727347aed7ffffb7ced5408827
Michael Jones <jonesmichael3.1415@gmail.com>
2025-02-02 18:51:54 -0500

    Making updates to the bar plot

--- src/archived_code/plot_convergence_bar_plot_nn_vs_rm.py
+++ src/archived_code/plot_convergence_bar_plot_nn_vs_rm.py
@@ -13,35 +13,35 @@ import numpy as np
 ITERATIONS = 20
 WAVEFRONTS_PER_BAR = 5000
 RANGES = (500, 20, 10)
-GAIN_VALUES = (-0.5, 0, 0)
-
-SCALE_FACTORS = [1, 0.5, 0.25, 0.125, 0.0625]
-# SCALING   Z2-3  Z4-8  Z9-24
-#       1    500    20     10
-#     0.5    250    10      5
-#    0.25    125     5    2.5
-#   0.125   62.5   2.5   1.25
-#  0.0625  31.25  1.25  0.625
-
-# The Y_NN model: weighted_aberration_ranges_local_v4_last
-# The Y_RM model: fixed_40nm_positive_and_negative
-
 THRESHOLD = 1e-9
-Y_NN = [5000, 5000, 5000, 5000, 5000]
-Y_RM = [273, 1251, 4452, 5000, 5000]
-
-# THRESHOLD = 5e-11
-# Y_NN = [4812, 4999, 5000, 5000, 5000]
-# Y_RM = [0, 0, 0, 0, 0]
+GAIN_VALUES = (-0.5, 0, 0)
+# NN: weighted_aberration_ranges_local_v4_last
+# RM: fixed_40nm_positive_and_negative
+# SCALING   Z2-3  Z4-8  Z9-24 |   NN    RM
+#  4        2000    80     40 |  395     ?
+#  3.5      1750    70     35 |    ?     ?
+#  3        1500    60     30 | 2076    14
+#  2.5      1250    50     25 | 3626    37
+#  2        1000    40     20 | 4886    66
+#  1.5       750    30     15 | 5000   124
+#  1.25      625    25   12.5 | 5000   182
+#  1         500    20     10 | 5000   273
+#  0.5       250    10      5 | 5000  1251
+#  0.25      125     5    2.5 | 5000  4452
+#  0.125    62.5   2.5   1.25 | 5000  5000
+#  0.0625  31.25  1.25  0.625 | 5000  5000
+SCALE_FACTORS = [4, 3, 2, 1, 0.5, 0.25, 0.125]
+Y_NN = [395, 2076, 4886, 5000, 5000, 5000, 5000]
+Y_RM = [2, 14, 66, 273, 1251, 4452, 5000]
 
 # ==============================================================================
 
 # Create the subplot
-fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+fig, ax = plt.subplots(1, 1, figsize=(12.5, 5))
 
 
 def _display_bars(y_values, offset, color, label):
-    bar = ax.bar(
+    bars = ax.bar(
         np.arange(len(y_values)) + offset,
         y_values,
         width=0.4,
@@ -49,45 +49,52 @@ def _display_bars(y_values, offset, color, label):
         label=label,
         color=color,
         edgecolor='#00364D',
-        linewidth=2,
+        linewidth=1,
     )
     # Add the percentage above every bar
     # https://stackoverflow.com/a/40491960
-    for rect in bar:
-        height = rect.get_height()
-        percentage = height / WAVEFRONTS_PER_BAR * 100
+    for bar in bars:
+        bar_height = bar.get_height()
+        percentage = bar_height / WAVEFRONTS_PER_BAR * 100
+        # Don't display the decimal places for 100%
+        if percentage == 100:
+            percentage_str = f'{percentage:.0f}'
+        else:
+            percentage_str = f'{percentage:.02f}'
         plt.text(
-            rect.get_x() + rect.get_width() / 2.0,
-            height,
-            f'{percentage:.02f}%',
+            bar.get_x() + bar.get_width() / 2.0,
+            bar_height + (-650 if percentage > 20 else 100),
+            f'{percentage_str}%\n({bar_height})',
             ha='center',
             va='bottom',
         )
 
 
 # Display the bars for both the NN and RM
-_display_bars(Y_NN, -0.2, '#43AA8B', 'Neural Network')
-_display_bars(Y_RM, 0.2, '#00B7FF', 'Respone Matrix')
+_display_bars(Y_NN, -0.2, '#FFAD99', 'Neural Network')
+_display_bars(Y_RM, 0.2, '#99C2FF', 'Respone Matrix')
 
 # Set the limits on the y axis along with the correct ticks
-ax.set_ylim([0, WAVEFRONTS_PER_BAR * 1.06])
+ax.set_ylim([0, WAVEFRONTS_PER_BAR * 1.035])
 ax.set_yticks(np.linspace(0, WAVEFRONTS_PER_BAR, 5))
+
+# The margin inside the actual plotting area needs to be smaller
+ax.margins(0.01)
 
 # The scale factor values should be the tick labels
 ax.set_xticklabels([None, *SCALE_FACTORS])
 
 # Set the titles and axis labels
 plt.title(
-    f'Captures for {ITERATIONS} Iteration Control Loops '
-    'on Static Aberration Input Signals\nGain Values $K_{P,I,D}$ = ' +
-    str(GAIN_VALUES),
+    f'{ITERATIONS} Iteration Control Loops on Static Aberration '
+    'Input Signals\nGain Factors $K_{P,I,D}$ = ' + str(GAIN_VALUES),
     pad=30,
 )
 ax.set_xlabel(
     'Scaling Factor ($k$)\nAberration Ranges Are Uniformly Random Between: '
-    r'$k\cdot\;[-x_Z, x_Z]$ nm RMS error where $x_{2-3}=$' + str(RANGES[0]) +
+    r'$k\cdot\;[-x_Z, x_Z]$ nm RMS error, where $x_{2-3}=$' + str(RANGES[0]) +
     '$, x_{4-8}=$' + str(RANGES[1]) + '$, x_{9-24}=$' + str(RANGES[2]))
-ax.set_ylabel('Wavefronts Captured\nThreshold: '
+ax.set_ylabel('Wavefronts Flattened\nTrue Error Threshold: '
               f'[-{THRESHOLD}, {THRESHOLD}]')
 
 # https://stackoverflow.com/a/43439132
