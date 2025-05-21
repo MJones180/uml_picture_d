03cf31545b97f7f47a769ee5e34893d56785b4ff
Michael Jones <jonesmichael3.1415@gmail.com>
2025-05-21 15:42:58 -0400

    Updated another plot

--- src/utils/plots/plot_zernike_cross_coupling_mat_animation.py
+++ src/utils/plots/plot_zernike_cross_coupling_mat_animation.py
@@ -1,6 +1,8 @@
 from matplotlib.animation import FuncAnimation, PillowWriter
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
+from utils.constants import PLOT_STYLE_FILE
 from utils.idl_rainbow_cmap import idl_rainbow_cmap
 
 
@@ -32,14 +34,18 @@ def plot_zernike_cross_coupling_mat_animation(
     animation_path : str
         Path to save the animation at, must be `.gif`.
     """
+    # Load in the style file
+    plt.style.use(PLOT_STYLE_FILE)
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(8, 8))
     ax.set_title(f'Cross-Coupling Matrix ({title_append})\n{identifier}')
     ax.set_ylabel('Output Zernike')
 
     # Create the initial plot and colorbar that will be updated
     im = ax.imshow(np.zeros_like(pred_groupings[0]), cmap=idl_rainbow_cmap())
-    fig.colorbar(im, ax=ax, label='nm RMS')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    fig.colorbar(im, cax=cax, orientation='vertical', label='nm RMS')
 
     # Invert the y-axis so that zero starts on the bottom
     ax.set_ylim(ax.get_ylim()[::-1])
