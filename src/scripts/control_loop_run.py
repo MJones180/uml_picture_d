9031f211da0800feeed56a861ca87498cc92f3c0
Michael Jones <jonesmichael3.1415@gmail.com>
2025-06-27 18:02:58 -0400

    Fixed a scope issue

--- src/scripts/control_loop_run.py
+++ src/scripts/control_loop_run.py
@@ -229,24 +229,24 @@ def control_loop_run(cli_args):
 
     print('Saving all Zernike RSS error plot.')
     plot_path = f'{output_path_ts_same}/rss_all_zernikes.png'
-    title = 'All Zernike RSS' + title_str
+    plot_title = 'All Zernike RSS' + title_str
     plot_control_loop_rss(
         true_error_history,
         meas_error_history,
-        title,
+        plot_title,
         cumulative_time,
         plot_path,
     )
 
     print('Saving Zernikes (excluding tip/tilt) RSS error plot.')
     plot_path = f'{output_path_ts_same}/rss_zernikes_no_tip_tilt.png'
-    title = 'Zernikes Excluding Tip/Tilt RSS' + title_str
+    plot_title = 'Zernikes Excluding Tip/Tilt RSS' + title_str
     # Grab all Zernike terms ignoring tip, tilt, and piston
     select_zernike_idxs = np.where(zernike_terms >= 4)[0]
     plot_control_loop_rss(
         true_error_history[:, select_zernike_idxs],
         meas_error_history[:, select_zernike_idxs],
-        title,
+        plot_title,
         cumulative_time,
         plot_path,
     )
