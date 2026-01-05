ee7e417859181a0fb24639825e33e866e201df28
Michael Jones <jonesmichael3.1415@gmail.com>
2026-01-05 16:22:20 -0500

    Added code to handle a 2D model output for the DM comparisons

--- src/scripts/dm_comparison.py
+++ src/scripts/dm_comparison.py
@@ -3,7 +3,8 @@ This script plots a comparison between actual and predicted DM commands.
 """
 
 import numpy as np
-from utils.constants import (ANALYSIS_P, DM_ACTIVE_IDXS, DM_COMPARISONS_P,
+from utils.constants import (ANALYSIS_P, DM_ACTIVE_COL_IDXS, DM_ACTIVE_IDXS,
+                             DM_ACTIVE_ROW_IDXS, DM_COMPARISONS_P,
                              EXTRA_VARS_F, PROC_DATA_P, RESULTS_F)
 from utils.hdf_read_and_write import read_hdf
 from utils.path import delete_dir, get_abs_path, make_dir
@@ -34,6 +35,12 @@ def dm_comparison_parser(subparsers):
         type=int,
         help=('number of pixels per row on the DM; it is expected that both '
               'DMs are square and are the same size'),
+    )
+    subparser.add_argument(
+        '--output-shape-correct',
+        action='store_true',
+        help=('the DM command output by the model is already in the correct '
+              'shape: 2 channels of a 2D pixel grid'),
     )
 
 
@@ -74,18 +81,45 @@ def dm_comparison(cli_args):
     extra_vars_path = f'{PROC_DATA_P}/{testing_ds_tag}/{EXTRA_VARS_F}'
     extra_vars_data = read_hdf(extra_vars_path)
 
-    step_ri('Grabbing active actuators on the DM')
-    dm1_active_idxs = extra_vars_data[DM_ACTIVE_IDXS(0)][:]
-    dm2_active_idxs = extra_vars_data[DM_ACTIVE_IDXS(1)][:]
+    if cli_args['output_shape_correct']:
+        step_ri('Grabbing active regions on the DM')
 
-    step_ri('Validating data')
-    active_actuators = dm1_active_idxs.shape[0]
-    if active_actuators != dm2_active_idxs.shape[0]:
-        terminate_with_message('DMs must be the same size')
-    elif 2 * active_actuators != outputs_model.shape[1]:
-        terminate_with_message('Model outputs have wrong number of actuators')
-    elif 2 * active_actuators != outputs_truth.shape[1]:
-        terminate_with_message('Truth outputs have wrong number of actuators')
+        def grab_dm_active_idxs(idx):
+            return (extra_vars_data[DM_ACTIVE_ROW_IDXS(idx)][:],
+                    extra_vars_data[DM_ACTIVE_COL_IDXS(idx)][:])
+
+        def _prep_dm_cmd(row_data, dm_idx):
+            row_idxs, col_idxs = grab_dm_active_idxs(dm_idx)
+            values_2d = row_data[dm_idx]
+            dm_cmd = values_2d[row_idxs]
+            dm_cmd = dm_cmd[:, col_idxs]
+            return dm_cmd
+
+    else:
+        step_ri('Grabbing active actuators on the DM')
+
+        def grab_dm_active_idxs(idx):
+            return extra_vars_data[DM_ACTIVE_ROW_IDXS(idx)][:]
+
+        step_ri('Validating data')
+        active_actuators = grab_dm_active_idxs(0).shape[0]
+        if active_actuators != grab_dm_active_idxs(1).shape[0]:
+            terminate_with_message('DMs must be the same size')
+        elif 2 * active_actuators != outputs_model.shape[1]:
+            terminate_with_message(
+                'Model outputs have the wrong number of actuators')
+        elif 2 * active_actuators != outputs_truth.shape[1]:
+            terminate_with_message(
+                'Truth outputs have the wrong number of actuators')
+
+        def _prep_dm_cmd(row_data, dm_idx):
+            if dm_idx == 0:
+                values_1d = row_data[:active_actuators]
+            else:
+                values_1d = row_data[active_actuators:]
+            dm_cmd = np.zeros(dm_size**2)
+            dm_cmd[grab_dm_active_idxs(dm_idx)] = values_1d
+            return dm_cmd.reshape(dm_size, dm_size)
 
     step_ri('DM specs')
     dm_size = cli_args['dm_size']
@@ -94,15 +128,10 @@ def dm_comparison(cli_args):
 
     step_ri('Generating comparison plots')
 
-    def _prep_dm_cmd(values_1d):
-        dm_cmd = np.zeros(dm_size**2)
-        dm_cmd[dm1_active_idxs] = values_1d
-        return dm_cmd.reshape(dm_size, dm_size)
-
     def _plot_row(outputs):
         return {
-            'dm1': _prep_dm_cmd(outputs[:active_actuators]),
-            'dm2': _prep_dm_cmd(outputs[active_actuators:]),
+            'dm1': _prep_dm_cmd(outputs, 0),
+            'dm2': _prep_dm_cmd(outputs, 1),
         }
 
     for idx in range(rows_to_plot):
