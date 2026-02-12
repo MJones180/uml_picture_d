fa919f6f241069465a2a06ba950ae95eccadfd11
Michael Jones <jonesmichael3.1415@gmail.com>
2026-02-11 20:15:50 -0500

    Updated the NN design

--- src/networks/dh_t36_1.py
+++ src/networks/dh_t36_1.py
@@ -1,17 +1,27 @@
 # `dh_t36_1` network { 1000 -> 1000 }.
-# Trainable parameters: 16,696,296
+# Trainable parameters: 12,497,896
 
 import torch
 import torch.nn as nn
 
 
-def _make_dense_block(in_features, out_features):
+def _make_dense_block(in_features, out_features, dropout=0):
     return nn.Sequential(
         nn.Linear(in_features, out_features, bias=False),
         nn.BatchNorm1d(out_features),
-        nn.LeakyReLU(),
-        nn.Dropout(0.5),
+        nn.LeakyReLU(0.2),
+        nn.Dropout(dropout),
     )
+
+
+class ResidualBlock(nn.Module):
+
+    def __init__(self, features, dropout=0.1):
+        super().__init__()
+        self.block = _make_dense_block(features, features, dropout)
+
+    def forward(self, x):
+        return x + self.block(x)
 
 
 class Network(nn.Module):
@@ -19,18 +29,16 @@ class Network(nn.Module):
     def example_input():
         return torch.rand((2, 1000))
 
-    def __init__(self):
+    def __init__(self, hidden_dim=2048, num_blocks=4):
         super().__init__()
-        self.layer_1 = _make_dense_block(1000, 2048)
-        self.layer_2 = _make_dense_block(2048, 2048)
-        self.layer_3 = _make_dense_block(2048, 2048)
-        self.layer_4 = _make_dense_block(2048, 2048)
+        self.in_layer = _make_dense_block(1000, 2048, 0)
+        self.res_block1 = ResidualBlock(2048, 0.2)
+        self.res_block2 = ResidualBlock(2048, 0.2)
         self.out_layer = nn.Linear(2048, 1000)
 
     def forward(self, x):
-        x = self.layer_1(x)
-        x = self.layer_2(x)
-        x = self.layer_3(x)
-        x = self.layer_4(x)
+        x = self.in_layer(x)
+        x = self.res_block1(x)
+        x = self.res_block2(x)
         x = self.out_layer(x)
         return x
--- src/networks/dh_t36_4.py
+++ src/networks/dh_t36_4.py
@@ -1,17 +1,27 @@
 # `dh_t36_4` network { 1000 -> 1000 }.
-# Trainable parameters: 92,128,232
+# Trainable parameters: 37,688,296
 
 import torch
 import torch.nn as nn
 
 
-def _make_dense_block(in_features, out_features):
+def _make_dense_block(in_features, out_features, dropout=0):
     return nn.Sequential(
         nn.Linear(in_features, out_features, bias=False),
         nn.BatchNorm1d(out_features),
-        nn.LeakyReLU(),
-        nn.Dropout(0.5),
+        nn.LeakyReLU(0.2),
+        nn.Dropout(dropout),
     )
+
+
+class ResidualBlock(nn.Module):
+
+    def __init__(self, features, dropout=0.1):
+        super().__init__()
+        self.block = _make_dense_block(features, features, dropout)
+
+    def forward(self, x):
+        return x + self.block(x)
 
 
 class Network(nn.Module):
@@ -19,22 +29,28 @@ class Network(nn.Module):
     def example_input():
         return torch.rand((2, 1000))
 
-    def __init__(self):
+    def __init__(self, hidden_dim=2048, num_blocks=4):
         super().__init__()
-        self.layer_1 = _make_dense_block(1000, 4096)
-        self.layer_2 = _make_dense_block(4096, 4096)
-        self.layer_3 = _make_dense_block(4096, 4096)
-        self.layer_4 = _make_dense_block(4096, 4096)
-        self.layer_5 = _make_dense_block(4096, 4096)
-        self.layer_6 = _make_dense_block(4096, 4096)
-        self.out_layer = nn.Linear(4096, 1000)
+        self.in_layer = _make_dense_block(1000, 2048, 0)
+        self.res_block1 = ResidualBlock(2048, 0.2)
+        self.res_block2 = ResidualBlock(2048, 0.2)
+        self.res_block3 = ResidualBlock(2048, 0.2)
+        self.res_block4 = ResidualBlock(2048, 0.2)
+        self.res_block5 = ResidualBlock(2048, 0.2)
+        self.res_block6 = ResidualBlock(2048, 0.2)
+        self.res_block7 = ResidualBlock(2048, 0.2)
+        self.res_block8 = ResidualBlock(2048, 0.2)
+        self.out_layer = nn.Linear(2048, 1000)
 
     def forward(self, x):
-        x = self.layer_1(x)
-        x = self.layer_2(x)
-        x = self.layer_3(x)
-        x = self.layer_4(x)
-        x = self.layer_5(x)
-        x = self.layer_6(x)
+        x = self.in_layer(x)
+        x = self.res_block1(x)
+        x = self.res_block2(x)
+        x = self.res_block3(x)
+        x = self.res_block4(x)
+        x = self.res_block5(x)
+        x = self.res_block6(x)
+        x = self.res_block7(x)
+        x = self.res_block8(x)
         x = self.out_layer(x)
         return x
