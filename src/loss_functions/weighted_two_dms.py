37f8ca801f710ab248827d4668676cf6136ff9ba
Michael Jones <jonesmichael3.1415@gmail.com>
2026-03-05 15:40:52 -0500

    Added new functionality to the two DM loss function

--- src/loss_functions/weighted_two_dms.py
+++ src/loss_functions/weighted_two_dms.py
@@ -17,6 +17,7 @@ class WeightedTwoDMs(nn.Module):
         apply_modified_log=None,
         apply_modified_exp=None,
         add_mae_loss=None,
+        add_exp_loss_weighting=None,
     ):
         """The WeightedTwoDMs class.
 
@@ -42,18 +43,32 @@ class WeightedTwoDMs(nn.Module):
             Apply a modified exp the outputs before the loss is calculated.
         add_mae_loss : float
             Add an MAE loss, scaled by alpha, to the MSE loss.
+        add_exp_loss_weighting : str
+            Add an exp weighting to the loss - boosts the gradient for
+            small errors. String of format 'alpha,beta' expected.
+
+        Notes
+        -----
+        Given delta (model_outputs - truth_outputs):
+        MSE:
+            - Loss: delta**2
+            - Gradient: 2 * delta
+        MSE with exp loss weighting:
+            - Loss: delta**2 + alpha * delta**2 * e**(-delta/beta)
+            - Gradient: 2 * delta + alpha * e**(-delta/beta)
+                                    * (2 * delta - delta**2/beta)
         """
         super().__init__()
 
-        def _grab_param(arg, desired_type):
+        def _grab_param(arg, desired_type=float):
             if arg is not None:
                 return desired_type(arg)
             return None
 
         # The conditional block declaration of `output_weights` gives the weight
         # of each output neuron associated with the first DM
-        linear_scaling = _grab_param(linear_scaling, float)
-        exp_scaling = _grab_param(exp_scaling, float)
+        linear_scaling = _grab_param(linear_scaling)
+        exp_scaling = _grab_param(exp_scaling)
         if linear_scaling:
             end_mode = outputs_per_dm
             linear_scaling_tail = _grab_param(linear_scaling_tail, int)
@@ -88,21 +103,31 @@ class WeightedTwoDMs(nn.Module):
             print('Will take the mean across each row')
 
         # Whether a modified log should be applied before determining the loss
-        self.apply_modified_log = _grab_param(apply_modified_log, float)
+        self.apply_modified_log = _grab_param(apply_modified_log)
         if self.apply_modified_log:
             print('Will apply a modified log before calculating the '
                   f'loss ({self.apply_modified_log} scaling)')
 
         # Whether a modified exp should be applied before determining the loss
-        self.apply_modified_exp = _grab_param(apply_modified_exp, float)
+        self.apply_modified_exp = _grab_param(apply_modified_exp)
         if self.apply_modified_exp:
             print('Will apply a modified exp before calculating the '
                   f'loss ({self.apply_modified_exp} base)')
 
         # Whether a combination of MAE and MSE loss should be used
-        self.add_mae_loss = _grab_param(add_mae_loss, float)
+        self.add_mae_loss = _grab_param(add_mae_loss)
         if self.add_mae_loss:
             print(f'Will add {self.add_mae_loss} MAE to the loss')
+
+        # Whether to add exponential weighting to the loss
+        self.add_exp_loss_weighting = _grab_param(add_exp_loss_weighting, str)
+        if self.add_exp_loss_weighting:
+            alpha, beta = self.add_exp_loss_weighting.split(',')
+            self.add_exp_loss_weighting_alpha = float(alpha)
+            self.add_exp_loss_weighting_beta = float(beta)
+            print('Will add exp weighting to the loss '
+                  f'(alpha = {self.add_exp_loss_weighting_alpha}, '
+                  f'beta = {self.add_exp_loss_weighting_beta})')
 
     def _apply_log_transformation(self, values):
         alpha = self.apply_modified_log
@@ -120,11 +145,20 @@ class WeightedTwoDMs(nn.Module):
         if self.apply_modified_exp:
             model_outputs = self._apply_exp_transformation(model_outputs)
             truth_outputs = self._apply_exp_transformation(truth_outputs)
-        difference = model_outputs - truth_outputs
-        loss_diff = difference**2
+        # While the absolute value is not needed for just MSE, it may be needed
+        # for other things, so it is computed now incase it's needed after
+        abs_delta = torch.abs(model_outputs - truth_outputs)
+        loss = abs_delta**2
         if self.add_mae_loss:
-            loss_diff = loss_diff + self.add_mae_loss * torch.abs(difference)
-        loss = self.output_weights * loss_diff
+            loss = loss + self.add_mae_loss * abs_delta
+        # Apply the exp loss weighting - makes small errors more important
+        if self.add_exp_loss_weighting:
+            loss_weights = 1.0 + (
+                self.add_exp_loss_weighting_alpha *
+                torch.exp(-abs_delta / self.add_exp_loss_weighting_beta))
+            loss = loss * loss_weights
+        # The loss weighted with respect to each given output neuron
+        weighted_loss = self.output_weights * loss
         if self.take_row_sum:
-            loss = loss.sum(axis=1)
-        return loss.mean()
+            weighted_loss = weighted_loss.sum(axis=1)
+        return weighted_loss.mean()
