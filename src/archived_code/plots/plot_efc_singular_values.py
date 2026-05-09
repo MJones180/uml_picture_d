3cbbe894c72511201ff7de494bdc8608ee2d5d57
Michael Jones <jonesmichael3.1415@gmail.com>
2026-05-09 11:55:41 -0400

    Updated the singular values plot

--- src/archived_code/plots/plot_efc_singular_values.py
+++ src/archived_code/plots/plot_efc_singular_values.py
@@ -1,32 +1,45 @@
 """
 Plot the singular values from SVD done on the EFC matrix.
-The file is located at `piccsim/output/svd_modes/dm1_w_matrix.fits`.
 """
 
-from astropy.io import fits
+from h5py import File
 import matplotlib.pyplot as plt
 import numpy as np
 
-FITS_PATH = '~/Downloads/dm1_w_matrix.fits'
+# The path to the HDF datafile
+HDF_PATH = '../data/raw/dm1_singular_values_flat/0_data.h5'
 # The number of modes to use
 NUM_MODES = 500
 
-with fits.open(FITS_PATH) as hdul:
-    singular_values = hdul['PRIMARY'].data
+# Load in the style file
+plt.style.use('plot_styling.mplstyle')
 
-singular_values = singular_values[:NUM_MODES]
-# Normalize to have a max of 1
-singular_values /= np.max(singular_values)
+fig, ax = plt.subplots(2, 2, figsize=(12, 10), sharex=True)
 
-x_points = np.arange(NUM_MODES)
 
-exp_scaling = 0.99**x_points
-linear_scaling = np.linspace(1, 0.1, 500)
+def _plot(axs_obj, data, title, set_tick_labels=True):
+    axs_obj.plot(data)
+    axs_obj.axhline(y=data[0], linestyle='--')
+    axs_obj.axhline(y=data[-1], linestyle='--')
+    axs_obj.set_yticks([data[0], data[-1]])
+    axs_obj.set_title(title)
+    axs_obj.set_xlabel('Singular Value')
+    axs_obj.set_ylabel('Magnitude', labelpad=-30)
+    if set_tick_labels:
+        axs_obj.set_yticklabels([f'{data[0]:0.3f}', f'{data[-1]:0.3f}'])
 
-plt.plot(x_points, singular_values, label='singular values')
-plt.plot(x_points, exp_scaling, label='exp (0.99) scaling')
-plt.plot(x_points, linear_scaling, label='linear [1, 0.1] scaling')
-plt.xlabel('Mode')
-plt.ylabel('Value')
-plt.legend()
-plt.show()
+
+singular_values = File(HDF_PATH)['singular_values'][:NUM_MODES]
+_plot(ax[0, 0], singular_values, '(A) Original Scale', False)
+
+singular_values = (singular_values - singular_values[-1]) / (
+    singular_values[0] - singular_values[-1])
+_plot(ax[0, 1], singular_values, '(B) Min-Max Normalization')
+
+singular_values = 0.1 + 0.9 * singular_values
+_plot(ax[1, 0], singular_values, '(C) Lower-Bound Scaling')
+
+singular_values /= np.mean(singular_values)
+_plot(ax[1, 1], singular_values, '(D) Divide by Mean')
+
+plt.savefig('singular_values_transformation.png')
