e3c700228fa0d8a5ae583b7e11c678b4ae0937b7
Michael Jones <jonesmichael3.1415@gmail.com>
2026-05-22 21:05:58 -0400

    Updated the networks

--- src/networks/dh_t78_h25do10dp30_h45do30dp30.py
+++ src/networks/dh_t78_h25do10dp30_h45do30dp30.py
@@ -11,20 +11,20 @@ import torch.nn as nn
 IN_DIM = 1000
 # Number of output neurons per head
 HEAD_OUT_DIM = 500
+# Number of neurons expanded out to
+OUTER_DIM = 768
 # Number of neurons for the bottleneck
 INNER_DIM = 512
 # Activation slope
 LEAKY_RELU = 0.2
+# LayerScale starting value
+GAMMA_INIT = 1e-2
 
 # ==============================================================================
 # HEAD 1 CONFIG OPTIONS
 # ==============================================================================
-# Number of neurons expanded out to
-HEAD_1_OUTER_DIM = 768
 # Number of blocks
 HEAD_1_DEPTH = 25
-# LayerScale starting value
-HEAD_1_GAMMA_INIT = 1e-2
 # Dropout rate
 HEAD_1_DROPOUT = 0.10
 # DropPath probability of dropping the last layer
@@ -35,12 +35,8 @@ HEAD_1_DP_PROBS = np.linspace(0, HEAD_1_DP_MAX_PROB, HEAD_1_DEPTH)
 # ==============================================================================
 # HEAD 2 CONFIG OPTIONS
 # ==============================================================================
-# Number of neurons expanded out to
-HEAD_2_OUTER_DIM = 768
 # Number of blocks
 HEAD_2_DEPTH = 45
-# LayerScale starting value
-HEAD_2_GAMMA_INIT = 1e-2
 # Dropout rate
 HEAD_2_DROPOUT = 0.30
 # DropPath probability of dropping the last layer
@@ -67,18 +63,18 @@ class DropPath(nn.Module):
 
 class BottleneckResidualBlock(nn.Module):
 
-    def __init__(self, outer_dim, gamma_init, dropout, drop_path_prob):
+    def __init__(self, dropout, drop_path_prob):
         super().__init__()
         self.block = nn.Sequential(
-            nn.BatchNorm1d(outer_dim),
+            nn.BatchNorm1d(OUTER_DIM),
             nn.LeakyReLU(LEAKY_RELU),
-            nn.Linear(outer_dim, INNER_DIM, bias=False),
+            nn.Linear(OUTER_DIM, INNER_DIM, bias=False),
             nn.BatchNorm1d(INNER_DIM),
             nn.LeakyReLU(LEAKY_RELU),
             nn.Dropout(dropout),
-            nn.Linear(INNER_DIM, outer_dim, bias=False),
+            nn.Linear(INNER_DIM, OUTER_DIM, bias=False),
         )
-        self.gamma = nn.Parameter(torch.full((outer_dim, ), gamma_init))
+        self.gamma = nn.Parameter(torch.full((OUTER_DIM, ), GAMMA_INIT))
         self.drop_path = DropPath(drop_path_prob)
 
     def forward(self, x):
@@ -93,31 +89,29 @@ class Network(nn.Module):
     def __init__(self):
         super().__init__()
         self.head_1 = nn.Sequential(
-            nn.Linear(IN_DIM, HEAD_1_OUTER_DIM, bias=False),
-            nn.BatchNorm1d(HEAD_1_OUTER_DIM),
+            nn.Linear(IN_DIM, OUTER_DIM, bias=False),
+            nn.BatchNorm1d(OUTER_DIM),
             *[
-                BottleneckResidualBlock(HEAD_1_OUTER_DIM, HEAD_1_GAMMA_INIT,
-                                        HEAD_1_DROPOUT,
+                BottleneckResidualBlock(HEAD_1_DROPOUT,
                                         HEAD_1_DP_PROBS[layer_idx])
                 for layer_idx in range(HEAD_1_DEPTH)
             ],
-            nn.BatchNorm1d(HEAD_1_OUTER_DIM),
+            nn.BatchNorm1d(OUTER_DIM),
             nn.LeakyReLU(LEAKY_RELU),
         )
         self.head_2 = nn.Sequential(
-            nn.Linear(IN_DIM, HEAD_2_OUTER_DIM, bias=False),
-            nn.BatchNorm1d(HEAD_2_OUTER_DIM),
+            nn.Linear(IN_DIM, OUTER_DIM, bias=False),
+            nn.BatchNorm1d(OUTER_DIM),
             *[
-                BottleneckResidualBlock(HEAD_2_OUTER_DIM, HEAD_2_GAMMA_INIT,
-                                        HEAD_2_DROPOUT,
+                BottleneckResidualBlock(HEAD_2_DROPOUT,
                                         HEAD_2_DP_PROBS[layer_idx])
                 for layer_idx in range(HEAD_2_DEPTH)
             ],
-            nn.BatchNorm1d(HEAD_2_OUTER_DIM),
+            nn.BatchNorm1d(OUTER_DIM),
             nn.LeakyReLU(LEAKY_RELU),
         )
-        self.head_1_out = nn.Linear(HEAD_1_OUTER_DIM, HEAD_OUT_DIM)
-        self.head_2_out = nn.Linear(HEAD_2_OUTER_DIM, HEAD_OUT_DIM)
+        self.head_1_out = nn.Linear(OUTER_DIM, HEAD_OUT_DIM)
+        self.head_2_out = nn.Linear(OUTER_DIM, HEAD_OUT_DIM)
 
     def forward(self, x):
         return self.head_1_out(self.head_1(x)), self.head_2_out(self.head_2(x))
