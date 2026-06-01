a2e22868e1f31a5d931ea85694d3f3e8d82950ae
Michael Jones <jonesmichael3.1415@gmail.com>
2026-06-01 10:33:18 -0400

    Added a new plot

---
+++ src/archived_code/plots/plot_dh_contrasts_spie_2026.py
@@ -0,0 +1,46 @@
+"""
+Plot DH contrasts between the the NN and EFC matrix after one iteration.
+For the SPIE Astronomical Telescopes + Instrumentation 2026 conference.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = {
+    'resp_mat': {
+        'label': 'EFC',
+        'color': '#43AA8B',
+        'marker': '*',
+        'data': [
+            1.1e-7, 8.4e-8, 8.0e-8, 9.4e-8, 1.0e-7, 1.2e-7, 8.9e-8, 1.2e-7,
+            1.1e-7, 9.8e-8
+        ],
+    },
+    'dh_v122_25': {
+        'label': 'NN',
+        'color': '#00B7FF',
+        'marker': '$+$',
+        'data': [v * 1e-9 for v in [10, 13, 8, 8, 7.5, 14, 10, 10, 10, 19]],
+    },
+}
+
+# Load in the style file
+plt.style.use('plot_styling.mplstyle')
+
+plt.figure(figsize=(10, 6))
+for values in data.values():
+    plt.scatter(
+        np.arange(10),
+        values['data'],
+        s=350,
+        label=values['label'],
+        color=values['color'],
+        marker=values['marker'],
+    )
+plt.legend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, 1.12))
+plt.yscale('log')
+plt.ylabel('Contrast', labelpad=-15)
+plt.xlabel('DH Realization')
+plt.locator_params(axis='x', nbins=10)
+plt.title('DH Contrast After First Iteration', pad=40)
+plt.savefig('dh_contrasts_spie_2026.png')
