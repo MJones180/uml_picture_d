8b7342d2285416608544cb757393fa88e1cde9e8
Michael Jones <jonesmichael3.1415@gmail.com>
2026-05-31 20:43:29 -0400

    Added a plot of the learning rate

---
+++ src/archived_code/plots/plot_learning_rate.py
@@ -0,0 +1,50 @@
+"""
+Plot the learning rate curve from a trained model.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+LR_PATH = '../output/trained_models/dh_v122_25/learning_rates.csv'
+
+# Load in the style file
+plt.style.use('plot_styling.mplstyle')
+
+data = np.loadtxt(LR_PATH, delimiter=',', skiprows=1)
+
+fig, ax = plt.subplots(figsize=(12, 6))
+
+
+def _add_vline(epoch):
+    ax.axvline(x=epoch, linestyle='--', linewidth=1.5, alpha=0.5)
+    ax.text(
+        x=epoch + 10,
+        y=0.02,
+        transform=ax.get_xaxis_transform(),
+        s=str(epoch),
+        va='center',
+        fontsize=14,
+    )
+
+
+def _add_hline(lr):
+    ax.axhline(y=lr, linestyle='--', linewidth=1.5, alpha=0.5)
+    ax.text(
+        x=0.002,
+        y=lr + 0.00025,
+        transform=ax.get_yaxis_transform(),
+        s=str(lr),
+        fontsize=14,
+    )
+
+
+_add_vline(80)
+_add_vline(680)
+_add_hline(3e-3)
+ax.plot(data[:, 1], linewidth=3)
+ax.set_yscale('log')
+ax.grid(True)
+ax.set_xlabel('Epoch')
+ax.set_ylabel('Learning Rate')
+ax.set_title('Learning Rate Schedule')
+plt.savefig('learning_rate.png')
