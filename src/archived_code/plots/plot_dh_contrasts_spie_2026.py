97f820bc56958b495b720142e56d255f46703039
Michael Jones <jonesmichael3.1415@gmail.com>
2026-06-02 11:10:13 -0400

    Updated the contrast plot

--- src/archived_code/plots/plot_dh_contrasts_spie_2026.py
+++ src/archived_code/plots/plot_dh_contrasts_spie_2026.py
@@ -12,8 +12,7 @@ data = {
         'color': '#43AA8B',
         'marker': '*',
         'data': [
-            1.1e-7, 8.4e-8, 8.0e-8, 9.4e-8, 1.0e-7, 1.2e-7, 8.9e-8, 1.2e-7,
-            1.1e-7, 9.8e-8
+            v * 1e-8 for v in [11, 8.4, 8, 9.4, 10, 12, 8.9, 12, 11, 9.8]
         ],
     },
     'dh_v122_25': {
@@ -27,9 +26,20 @@ data = {
 # Load in the style file
 plt.style.use('plot_styling.mplstyle')
 
-plt.figure(figsize=(10, 4))
+
+def math_notation(value):
+    coeff, exp = f'{value:.3e}'.split('e')
+    return rf'${coeff} \cdot 10^{{{int(exp)}}}$'
+
+
+fig, ax1 = plt.subplots(figsize=(10, 4))
+mean_values = []
 for values in data.values():
-    plt.scatter(
+    # Compute the geometric mean; instead of calculating prod(x)^(1/N), the
+    # following computational trick can be used
+    mean_value = 10**np.mean(np.log10(values['data']))
+    mean_values.append(mean_value)
+    ax1.scatter(
         np.arange(10),
         values['data'],
         s=350,
@@ -37,10 +47,21 @@ for values in data.values():
         color=values['color'],
         marker=values['marker'],
     )
-plt.legend(loc='upper center', ncol=4, bbox_to_anchor=(0.5, 1.175))
-plt.yscale('log')
-plt.ylabel('Contrast', labelpad=-15)
-plt.xlabel('DH Realization')
-plt.locator_params(axis='x', nbins=10)
-plt.title('DH Contrast After First Iteration', pad=40)
+    ax1.axhline(y=mean_value, linestyle='--', color=values['color'])
+
+ax1.set_title('DH Contrast After First Iteration', pad=40)
+ax1.legend(loc='upper center', ncol=2, bbox_to_anchor=(0.5, 1.175))
+ax1.set_yscale('log')
+ax1.set_ylabel('Contrast', labelpad=-18)
+ax1.set_xlabel('DH Realization')
+ax1.locator_params(axis='x', nbins=10)
+
+ax2 = ax1.twinx()
+ax2.set_ylim(ax1.get_ylim())
+ax2.set_yscale('log')
+ax2.set_ylabel('Mean\nContrast', labelpad=-40)
+ax2.set_yticks(mean_values)
+ax2.set_yticklabels([math_notation(val) for val in mean_values])
+ax2.minorticks_off()
+
 plt.savefig('dh_contrasts_spie_2026.png')
