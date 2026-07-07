a68477bf8259d6caa3f34cb29fc215045fae1a30
Michael Jones <jonesmichael3.1415@gmail.com>
2026-07-07 09:15:05 -0400

    Updated the modes creation script

--- src/scripts/create_pca_basis_modes.py
+++ src/scripts/create_pca_basis_modes.py
@@ -1,8 +1,10 @@
 import numpy as np
 import torch
-from utils.constants import DATA_F, RAW_DATA_P
+from utils.cli_args import save_cli_args
+from utils.constants import DARK_ZONE_MASK, DATA_F, RAW_DATA_P
 from utils.hdf_read_and_write import HDFWriteModule, read_hdf
 from utils.load_raw_sim_data import raw_sim_data_chunk_paths
+from utils.path import make_dir
 from utils.printing_and_logging import step_ri, title
 
 
@@ -28,12 +30,12 @@ def create_pca_basis_modes_parser(subparsers):
     )
     subparser.add_argument(
         '--table-names',
+        nargs='*',
         help=('names of the tables in the raw data files; if multiple tables '
               'are given, then the data will be concat in the order passed'),
     )
     subparser.add_argument(
         '--mask',
-        nargs=2,
         help=('apply a mask to the data; two args expected: '
               'mask tag; HDF table name'),
     )
@@ -55,10 +57,9 @@ def create_pca_basis_modes(cli_args):
     mask = cli_args.get('mask')
     if mask:
         step_ri('Will apply a mask to the data')
-        mask_tag, mask_table = mask
-        mask_path = raw_sim_data_chunk_paths(mask_tag)[0]
-        print(f'Loading in the mask from {mask_path} ({mask_table})')
-        mask_data = read_hdf(mask_path)[mask_table][:]
+        mask_path = raw_sim_data_chunk_paths(mask)[0]
+        print(f'Loading in the mask from {mask_path}')
+        mask_data = read_hdf(mask_path)[DARK_ZONE_MASK][:]
 
     step_ri('Loading in raw datafiles')
     all_table_data = {table_name: [] for table_name in table_names}
@@ -73,10 +74,11 @@ def create_pca_basis_modes(cli_args):
                 all_table_data[table_name].extend(table_data)
 
     step_ri('Joining together all the data')
-    for table_name, table_data in table_names:
+    for table_name, table_data in all_table_data.items():
         table_data = np.array(table_data)
         print(f'{table_name}: {table_data.shape}')
-    all_table_data = np.concat(*[all_table_data[name] for name in table_names],
+    # Convert from a dict to a numpy array containing the combined data
+    all_table_data = np.concat([all_table_data[name] for name in table_names],
                                axis=-1)
     print(f'Merged shape: {all_table_data.shape}')
 
@@ -97,6 +99,11 @@ def create_pca_basis_modes(cli_args):
     print(f'Modes shape: {modes.shape}')
 
     step_ri('Writing out modes')
-    datafile_path = f'{RAW_DATA_P}/{output_tag}/0_{DATA_F}'
+    out_dir = f'{RAW_DATA_P}/{output_tag}'
+    print(f'Creating {out_dir}')
+    make_dir(out_dir)
+    # Write out the CLI args that this script was called with
+    save_cli_args(out_dir, cli_args, 'create_pca_basis_modes')
+    datafile_path = f'{out_dir}/0_{DATA_F}'
     print(f'Path: {datafile_path}')
     HDFWriteModule(datafile_path).create_and_write_hdf_simple({'modes': modes})
