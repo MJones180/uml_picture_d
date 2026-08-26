b278c4f8f07b7871ae047955c9f07e76f0823cb2
Michael Jones <jonesmichael3.1415@gmail.com>
2026-08-26 12:35:22 -0400

    Added a script to plot just lines

---
+++ src/utils/plots/plot_line.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from utils.constants import PLOT_STYLE_FILE
+
+
+def plot_line(
+    data,
+    title,
+    xlabel,
+    ylabel,
+    plot_path,
+    labels=None,
+    show_grid=False,
+):
+    # Load in the style file
+    plt.style.use(PLOT_STYLE_FILE)
+    plt.clf()
+    plt.figure(figsize=(14, 6))
+    # Treat all data as 2D
+    if len(np.asarray(data).shape) == 1:
+        data = [data]
+    for idx, line in enumerate(data):
+        label = '' if labels is None else labels[idx]
+        plt.plot(line, label=label)
+    if labels is not None:
+        plt.legend()
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    if show_grid:
+        plt.grid()
+    plt.savefig(plot_path)
